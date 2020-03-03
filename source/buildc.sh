18639d90e3773808c740bf8f6974a4f4529084c3
duffyj <johnduffyx@outlook.com>
2020-03-03 05:01:38 -0500

    build changes.

--- source/buildc.sh
+++ source/buildc.sh
@@ -8,12 +8,14 @@ if [ $all -eq 1 ]; then
 	../../Framework/cmake/buildc.sh ../../Ssl/source $type $clean || exit 1;
 	../../Framework/cmake/buildc.sh ../../Google/source $type $clean || exit 1;
 fi
-output=.obj/$type
-if [ ! -d $output ]; then mkdir $output; fi;
-cd $output
+if [ ! -d .obj ];	then mkdir .obj; clean=1; fi;
+cd .obj;
+if [ ! -d $type ]; then mkdir $type; clean=1; fi;
+cd $type;
+if [ ! -f CMakeCache.txt ]; then clean=1; fi;
 
 if [ $clean -eq 1 ]; then
-	rm CMakeCache.txt;
+	rm -f CMakeCache.txt;
 	cmake -DCMAKE_BUILD_TYPE=$type  ../.. > /dev/null;
 	make clean;
 fi
